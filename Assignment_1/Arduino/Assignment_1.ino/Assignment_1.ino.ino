dfaf8621ff42f6697aed4567c7c299547c70a7d7
mcoenen1994 <maikelcoenen@icloud.com>
2017-09-07 18:06:36 +0200

    signed to unsigned and new multiplication
    Changed the signed integers to unsigned what improved the excecution time from 11800 to 7035. Also implemented new multiplication code which improved the time to 6740.

--- Assignment_1/Arduino/Assignment_1.ino/Assignment_1.ino.ino
+++ Assignment_1/Arduino/Assignment_1.ino/Assignment_1.ino.ino
@@ -16,7 +16,7 @@ void loop() {
   //
   unsigned long startTime = micros();
 
-  int8_t matrixX[NR_MATRIX_ROWS][NR_MATRIX_COLS] =
+  uint8_t matrixX[NR_MATRIX_ROWS][NR_MATRIX_COLS] =
   {{28,  122,  80,   42,   54,   122,  98,   42,   99,   58,   124,  29,   21},
   {113, 85,   30,   35,   41,   98,   103,  68,   15,   50,   31,   80,   54},
   {47,  37,   23,   96,   59,   47,   84,   26,   84,   72,   51,   118,  119},
@@ -31,7 +31,7 @@ void loop() {
   {25,  109,  74,   3,    126,  56,   99,   15,   69,   73,   76,   19,   97},
   {59,  84,   102,  53,   30,   34,   33,   105,  75,   102,  60,   121,  93}};
   
-  int8_t matrixY[NR_MATRIX_ROWS][NR_MATRIX_COLS] =
+  uint8_t matrixY[NR_MATRIX_ROWS][NR_MATRIX_COLS] =
   {{102,  61,   111,  79,   99,   3,    25,   50,   33,   48,   5,    94,   28},
   {106, 89,   35,   37,   112,  51,   13,   70,   3,    110,  31,   7,    99},
   {65,  115,  94,   68,   95,   114,  34,   34,   64,   1,    11,   66,   126},
@@ -46,40 +46,70 @@ void loop() {
   {70,  108,  69,   12,   0,    80,   115,  107,  71,   54,   5,    57,   3},
   {123, 72,   56,   5,    30,   45,   2,    11,   124,  84,   63,   47,   104}};
   
-  int32_t matrixR[NR_MATRIX_ROWS][NR_MATRIX_COLS] = {0};
+  uint32_t matrixR[NR_MATRIX_ROWS][NR_MATRIX_COLS] = {0};
 
-  int8_t *x = (int8_t*)matrixX;
-  int8_t *y = (int8_t*)matrixY;
-  int32_t *r = (int32_t*)matrixR;
+  uint8_t *x = (int8_t*)matrixX;
+  uint8_t *y = (int8_t*)matrixY;
+  uint32_t *r = (int32_t*)matrixR;
 
-  uint8_t nrRows = NR_MATRIX_ROWS;
-  uint8_t nrCols = NR_MATRIX_COLS;
-  uint8_t nrElements = NR_MATRIX_ELEMENTS;
+  register uint8_t nrRows = NR_MATRIX_ROWS;
+  register uint8_t nrCols = NR_MATRIX_COLS;
+  register uint8_t nrElements = NR_MATRIX_ELEMENTS;
 
-  for(nrRows=0; nrRows<NR_MATRIX_ROWS; nrRows++){
-        for(nrCols=0; nrCols<NR_MATRIX_COLS; nrCols++){
+  register uint8_t i;
+  register uint8_t j;
+  register uint8_t k;
 
-            /*Multiplication.*/
-            *r += *x * *y;
-            *r += *(x+1) * *(y+13);
-            *r += *(x+2) * *(y+26);
-            *r += *(x+3) * *(y+39);
-            *r += *(x+4) * *(y+52);
-            *r += *(x+5) * *(y+65);
-            *r += *(x+6) * *(y+78);
-            *r += *(x+7) * *(y+91);
-            *r += *(x+8) * *(y+104);
-            *r += *(x+9) * *(y+117);
-            *r += *(x+10) * *(y+130);
-            *r += *(x+11) * *(y+143);
-            *r += *(x+12) * *(y+156);
-
-            ++y;
-            ++r;
-        }
-        x+=13;
-        y=(int8_t*)matrixY;
+  for(i = 0; i < NR_MATRIX_ROWS; i++)
+  {
+    for(j = 0; j < NR_MATRIX_COLS; j++)
+    {
+      x += (NR_MATRIX_COLS * i);
+      y += j;
+      for(k = 0; k < NR_MATRIX_COLS; k++)
+      {
+        *r += *x * *y;
+        x++;
+        y += NR_MATRIX_COLS;
+      }
+      r++;
+      x = (int8_t*)matrixX;
+      y = (int8_t*)matrixY;
     }
+  }
+
+
+//  for(nrRows=0; nrRows<NR_MATRIX_ROWS; nrRows++){
+//        for(nrCols=0; nrCols<NR_MATRIX_COLS; nrCols++){
+//          for(i = 0; i < NR_MATRIX_ROWS; i++){
+//
+//            *r = *x * *y;
+//            //*r += *(x+a) * *(y+b);
+//
+//            /*Multiplication.*/
+////            *r += *x * *y;
+////            *r += *(x+1) * *(y+13);
+////            *r += *(x+2) * *(y+26);
+////            *r += *(x+3) * *(y+39);
+////            *r += *(x+4) * *(y+52);
+////            *r += *(x+5) * *(y+65);
+////            *r += *(x+6) * *(y+78);
+////            *r += *(x+7) * *(y+91);
+////            *r += *(x+8) * *(y+104);
+////            *r += *(x+9) * *(y+117);
+////            *r += *(x+10) * *(y+130);
+////            *r += *(x+11) * *(y+143);
+////            *r += *(x+12) * *(y+156);
+//
+//            ++a;
+//            b += 13;
+//            ++y;
+//            ++r;
+//          }
+//        }
+//        x+=13;
+//        y=(int8_t*)matrixY;
+//    }
 
     /*Matrices back to base address*/
     x = (int8_t*)matrixX;
