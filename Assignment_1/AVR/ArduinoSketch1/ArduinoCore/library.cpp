93db5f8d202218c52d1d00576ad84100e954a7a8
MatthijsvanDelft <Matthijsvandelft@hotmail.com>
2017-09-12 08:10:30 +0200

    Added AVR project files
    Added AVR project files for further creation in Atmel Studio.

---
+++ Assignment_1/AVR/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 11-9-2017 16:56:56
+ * Author : Matthijs
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
